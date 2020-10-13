64ed54eb1dfcd151f72bb50e8ac06f473b006e0f
ceps <cepsdev@hotmail.com>
2020-10-13 23:25:33 +0200

    Preliminary benchmarking added. It takes roughly 274 times longer to write data with activated type compression than the same data without compressed type information. This applies to highly redundant type information, e.g. a picture consisting of millions of RGB triplets.

--- compressed_ast.cpp
+++ compressed_ast.cpp
@@ -38,8 +38,8 @@ struct ast_t{
 
 	std::uint16_t ngram_counter_ = 0;
 
-	std::vector<std::uint16_t> ngrams;
-	std::vector<std::size_t> codes2ngrams;
+	mutable std::vector<std::uint16_t> ngrams;
+	mutable std::vector<std::size_t> codes2ngrams;
 
 	size_t mutable cur_ofs_ = 0;
 	size_t mutable cur_type_ofs_ = 0;
@@ -47,6 +47,41 @@ struct ast_t{
 	size_t mutable type_idx = 0;
 
 	static bool is_composite(std::atomic_uint16_t code) { return code & 0x8000; }
+
+	std::size_t max_ngram_len()  {
+		size_t r = 0;
+		size_t c = 0;
+		if (!ngrams.size())
+			prepare_reading();
+		for (auto const & e : ngrams){
+			if (e == TAG_EOF)
+			{
+				if (c > r) r = c;
+				c = 0;
+			}
+			else ++c;
+		}
+		if (c > 0 && c > r) return c;
+		return r;
+	}
+
+	std::size_t ngram_count() {
+		size_t r = 0;
+		size_t c = 0;
+
+		if (!ngrams.size())
+			prepare_reading();
+		for (auto const& e : ngrams) {
+			if (e == TAG_EOF)
+			{
+				if (c > 0) ++r;
+				c = 0;
+			}
+			else ++c;
+		}
+		if (c > 0 ) return c+1;
+		return r;
+	}
 
 	bool get(std::uint16_t & elem_type, char** data, size_t& len) const {
 		if (current_data_beg_ <= cur_ofs_) return false;
@@ -89,7 +124,7 @@ struct ast_t{
 
 	void reset() { cur_ofs_ = 0; cur_type_ofs_ = 0; current_type_len_ = type_info_cur_; }
 
-	void prepare_reading() {
+	void prepare_reading()  {
 		codes2ngrams.resize(ngram_counter_ + 1);
 		for (auto const& e : lookup_) {
 			codes2ngrams[e.second] = ngrams.size();
@@ -103,7 +138,6 @@ struct ast_t{
 		if (run_.length() == 0) return;
 		if (run_.length() == 1) *(((std::uint16_t*)type_info_) + type_info_cur_++) = run_[0];
 		else *(((std::uint16_t*)type_info_) + type_info_cur_++) = 0x8000 | lookup_[run_] ;
-		prepare_reading();
 	}
 
 	void write_type_tag(std::uint16_t tag) {
@@ -121,32 +155,48 @@ struct ast_t{
 			}
 		}
 	}
-	void write_int(std::int32_t value, std::string name = std::string{}) {
-		write_type_tag(TAG_INT32);
+
+	void write_int_data(std::int32_t value, std::string name = std::string{}) {
 		*(std::int32_t*)(data_ + current_data_beg_) = value;
 		current_data_beg_ += sizeof(std::int32_t);
 	}
 
-	void write_int16(std::int16_t value, std::string name = std::string{}) {
-		write_type_tag(TAG_INT16);
+	void write_int(std::int32_t value, std::string name = std::string{}) {
+		write_type_tag(TAG_INT32);
+		write_int_data(value,name);
+	}
+
+	void write_int16_data(std::int16_t value, std::string name = std::string{}) {
 		*(std::int16_t*)(data_ + current_data_beg_) = value;
 		current_data_beg_ += sizeof(std::int16_t);
 	}
 
-	void write_int64(std::int64_t value, std::string name = std::string{}) {
-		write_type_tag(TAG_INT64);
+	void write_int16(std::int16_t value, std::string name = std::string{}) {
+		write_type_tag(TAG_INT16);
+		write_int16_data(value, name);
+	}
+
+	void write_int64_data(std::int64_t value, std::string name = std::string{}) {
 		*(std::int64_t*)(data_ + current_data_beg_) = value;
 		current_data_beg_ += sizeof(std::int64_t);
 	}
 
-	void write_double(double value, std::string name = std::string{}) {
-		write_type_tag(TAG_DOUBLE);
+	void write_int64(std::int64_t value, std::string name = std::string{}) {
+		write_type_tag(TAG_INT64);
+		write_int64_data(value,name);
+	}
+
+	void write_double_data(double value, std::string name = std::string{}) {
 		*(double*)(data_ + current_data_beg_) = value;
 		current_data_beg_ += sizeof(double);
 	}
 
-	void write_ascii(std::string const & value, std::string name = std::string{}) {
-		write_type_tag(TAG_ASCII);
+	void write_double(double value, std::string name = std::string{}) {
+		write_type_tag(TAG_DOUBLE);
+		write_double_data(value,name);
+	}
+
+	void write_ascii_data(std::string const& value, std::string name = std::string{}) {
 		*(std::size_t*)(data_ + current_data_beg_) = value.length();
 		current_data_beg_ += sizeof(std::size_t);
 		if (value.length() == 0) return;
@@ -154,6 +204,11 @@ struct ast_t{
 		current_data_beg_ += value.length();
 		*(char*)(data_ + current_data_beg_) = 0;
 		++current_data_beg_;
+	}
+
+	void write_ascii(std::string const & value, std::string name = std::string{}) {
+		write_type_tag(TAG_ASCII);
+		write_ascii_data(value,name);
 	}
 
 	void down() {
@@ -183,12 +238,8 @@ std::ostream& operator << (std::ostream& os, ast_t& ast) {
 	std::uint16_t type; std::size_t len; char* data;
 	ast.reset();
 	for (; ast.get(type, &data, len);) {
-		std::cout << "#" << type <<"#\n";
 		if (!ast_t::is_composite(type))
 			handle_simple_types(type,data,len);
-		else {
-			std::cerr << "?";
-		}
 		os << "\n";
 	}
 	return os;
@@ -210,25 +261,98 @@ void print_debug_info(std::ostream& os, ast_t const& ast) {
 	for (auto const& e : ast.ngrams) os << e << ",";
 }
 
+
+#include <chrono>
+std::tuple<std::chrono::microseconds, std::chrono::microseconds,std::size_t,std::size_t,std::size_t,std::size_t,std::size_t> speed_test(unsigned int loops = 100) {
+	size_t data_size = 100*1024 * 1024;
+	size_t typeinfo_size = data_size;
+	char* data_buffer = new char[data_size]{ 0 };
+	char* type_buffer = new char[typeinfo_size]{ 0 };
+	std::size_t bytes_written = 0;
+	size_t max_ngram_len = 0;
+	size_t ngram_count = 0;
+	size_t payload = 0;
+	size_t type_info_size = 0;
+
+	{
+		ast_t ast{ data_buffer, type_buffer, data_size, typeinfo_size };
+		for (auto j = 0; j < 2*1024 * 1024; ++j) {
+			ast.write_int(j);
+			ast.write_int(j);
+			ast.write_int(j);
+			ast.write_double(++j + 0.5);
+			ast.write_ascii("Hello!");
+			ast.write_int(++j);
+			ast.write_ascii("Hello!");
+		}
+		max_ngram_len = ast.max_ngram_len();
+		ngram_count = ast.ngram_count();
+		bytes_written = (payload = ast.current_data_beg_)*loops;
+		type_info_size = ast.type_info_cur_;
+	}
+
+	auto t1_1 = std::chrono::high_resolution_clock::now();
+
+	for (unsigned int loop_ctr = 0; loop_ctr < loops; ++loop_ctr) {
+		ast_t ast{ data_buffer, type_buffer, data_size, typeinfo_size };
+		for (auto j = 0; j < 2*1024*1024; ++j) {
+			ast.write_int(j);
+			ast.write_int(j);
+			ast.write_int(j);
+			ast.write_double(++j + 0.5);
+			ast.write_ascii("Hello!");
+			ast.write_int(++j);
+			ast.write_ascii("Hello!");
+		}
+		ast.write_eof();
+	}
+
+	auto t2_1 = std::chrono::high_resolution_clock::now();
+
+	auto runtime_with_compression = std::chrono::duration_cast<std::chrono::microseconds>(t2_1 - t1_1);
+
+	auto t1_2 = std::chrono::high_resolution_clock::now();
+
+	for (unsigned int loop_ctr = 0; loop_ctr < loops; ++loop_ctr) {
+		ast_t ast{ data_buffer, type_buffer, data_size, typeinfo_size };
+		for (auto j = 0; j < 2 * 1024 * 1024; ++j) {
+			ast.write_int_data(j);
+			ast.write_int_data(j);
+			ast.write_int_data(j);
+			ast.write_double_data(++j + 0.5);
+			ast.write_ascii_data("Hello!");
+			ast.write_int_data(++j);
+			ast.write_ascii_data("Hello!");
+		}
+		ast.write_eof();
+	}
+
+	auto t2_2 = std::chrono::high_resolution_clock::now();
+	auto runtime_no_compression = std::chrono::duration_cast<std::chrono::microseconds>(t2_2 - t1_2);
+
+	return { runtime_no_compression , runtime_with_compression, bytes_written, max_ngram_len,ngram_count,payload,type_info_size };
+}
+
 int main()
 {
-	char* data_buffer = new char[32768]  { 0 };
-	char* type_buffer = new char[2048]  { 0 };
+	{
+		unsigned int loops = 10;
+		auto result = speed_test(loops);
+		auto mib_written = (double)std::get<2>(result) / (double)(1024 * 1024);
+		auto avg_time_compressed = ((double)std::get<1>(result).count() / 1000000.0) / (double)loops;
+		auto avg_time_uncompressed = ((double)std::get<0>(result).count() / 1000000.0) / (double)loops;
+		auto max_ngram_len = std::get<3>(result);
+		auto ngram_count = std::get<4>(result);
+		auto payload = std::get<5>(result);
+		auto typeinfo_size = std::get<6>(result);
 
-	ast_t ast{ data_buffer, type_buffer, 4096, 2048 };
-	for (auto j = 0; j < 256; ++j) {
-		ast.write_int(j);
-		ast.write_double(++j);
-		ast.write_ascii("Hello!");
-		ast.write_int(++j);
+		std::cout << "Test 1:\n";
+		std::cout << "Length (in primitive 1-grams) of longest n-gram:" << max_ngram_len << "\n";
+		std::cout << "Number of composite n-grams:" << ngram_count<< "\n";
+		std::cout << "Payload (size of raw data):" << payload << " bytes\n";
+		std::cout << "Type Information :" << typeinfo_size << " bytes\n";
+		std::cout << "Bytes written (total):" << mib_written << " MiB\n";
+		std::cout << "Elapsed Time per write (with type compression, on average):" << avg_time_compressed << " seconds" << " that is "<< mib_written/ (loops*avg_time_compressed)  << " MiB/sec\n";
+		std::cout << "Elapsed Time per write (without type compression, on average):" << avg_time_uncompressed << " seconds" << " that is " << mib_written / (loops*avg_time_uncompressed) << " MiB/sec\n";
 	}
-	ast.write_eof();
-
-	//print_debug_info(std::cout, ast);
-
-
-	std::cout << "Decompressed:\n";
-	std::cout << ast << "\n\n\n";
-
-
 }
